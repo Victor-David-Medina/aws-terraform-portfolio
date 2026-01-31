24ec994f1db95be05d2e38281bbf758fc585f2f4
David Medina <V.DavidMedina@gmail.com>
2026-01-31 15:07:51 -0500

    phase 1B: Complete VPC with subnets, IGW, route tables

---
+++ 02-vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "project_name" {
+  default = "vdm-cloud-ops"
+}
