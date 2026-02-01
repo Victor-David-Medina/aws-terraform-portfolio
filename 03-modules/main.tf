790396b8a7705ea2ef38f3f097e816b03f02c392
David Medina <V.DavidMedina@gmail.com>
2026-02-01 14:46:36 -0500

    Phase 2B: S3 remote state with native locking

--- 03-modules/main.tf
+++ 03-modules/main.tf
@@ -1,18 +1,27 @@
-#main .tf (ROOT folder)
+# 03-modules/main.tf
+
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0 " 
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket       = "vdm-terraform-state-a31c60f6"
+    key          = "03-modules/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-#Create DEV VPC
+# Create DEV VPC
 module "vpc_dev" {
   source       = "./modules/vpc"
   environment  = "dev"
@@ -20,10 +29,10 @@ module "vpc_dev" {
   vpc_cidr     = "10.0.0.0/16"
 }
 
-# Create PROD VPC (same module!)
+# Create PROD VPC
 module "vpc_prod" {
-    source       = "./modules/vpc"
-    environment  = "prod"
-    project_name = "vdm"
-    vpc_cidr     = "10.1.0.0/16"
+  source       = "./modules/vpc"
+  environment  = "prod"
+  project_name = "vdm"
+  vpc_cidr     = "10.1.0.0/16"
 }
--- backend-setup/main.tf
+++ backend-setup/main.tf
@@ -1,29 +1,48 @@
-#backend-setup/main.tf
-  terraform {
-    required_providers {
-        aws = { source = "hashicorp/aws", version = "~> 5.0" }
+# backend-setup/main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
+}
 
-provider "aws" { region = "us-east-1" }
-  
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "vdm-terraform-state-DM"
-    lifecycle { prevent_destroy = true }
-    tags = { Name = "Terraform State" }
+  bucket = "vdm-terraform-state-${random_id.suffix.hex}"
+
+  tags = {
+    Name      = "Terraform State Bucket"
+    ManagedBy = "Terraform"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "enabled" {
-    bucket = aws_s3_bucket.terraform_state.id
-    versioning_configuration { status = "Enabled" }
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-    name         = "vdm-terraform-locks"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key     = "LockID"
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "bucket_name" {
+  value       = aws_s3_bucket.terraform_state.id
+  description = "Use this in your backend config!"
 }
